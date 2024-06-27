bb2a4dcd3373064a3b7dde2c2bcb2d469b0b3297
Sophia Katsuno <sophia.katsuno@gmail.com>
2024-06-26 17:02:24 -0700

    draft of topbar

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import Topbar from "../../components/topbar/Topbar"
 
 export default function () {
   return (
-    <div>
-      Homepage
-    </div>
+    <Topbar />
+    
   )
 }
