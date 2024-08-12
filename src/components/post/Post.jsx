aa465e63dc0238b64153a1263fead57e252f778b
Sophia Katsuno <sophia.katsuno@gmail.com>
2024-08-12 12:47:07 -0700

    incorporate dynamic data, add online and close friend component, debug public/assets/photo route

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,37 +1,48 @@
 import "./post.css";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Users } from "../../dummyData";
 
-export default function Post() {
+export default function Post({ post }) {
   return (
     <div className="post">
-      <div className="postTop">
-        <div className="postTopLeft">
-          <img
-            className="postProfileImg"
-            src="/assets/person/1.jpeg"
-            alt=""
-          />
-          <span className="postUsername">Sophia K</span>
-          <span className="postDate">5 mins ago</span>
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              className="postProfileImg"
+              src={
+                Users.filter((u) => u.id === post.userId)[0]
+                  .profilePicture
+              }
+              alt=""
+            />
+            <span className="postUsername">
+              {Users.filter((u) => u.id === post.userId)[0].username}
+            </span>
+            <span className="postDate">{post.date}</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVertIcon />
+          </div>
         </div>
-        <div className="postTopRight">
-          <MoreVertIcon />
+        <div className="postCenter">
+          <span className="postText"> {post?.desc}</span>
+          <img className="postImg" src={post.photo} alt="" />
         </div>
-      </div>
-      <div className="postCenter">
-        <span className="postText"> My first post :D</span>
-        <img className="postImg" src="/assets/post/1.jpeg" alt="" />
-      </div>
-      <div className="postBottom">
-        <div className="postBottomLeft">
-          <img className="likeIcon" src="assets/like.png" alt="" />
-          <img className="likeIcon" src="assets/heart.png" alt="" />
-          <span className="postLikeCounter">
-            32 people liked this
-          </span>
-        </div>
-        <div className="postBottomRight">
-          <span className="postCommentText"> 9 comments</span>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="likeIcon" src="assets/like.png" alt="" />
+            <img className="likeIcon" src="assets/heart.png" alt="" />
+            <span className="postLikeCounter">
+              {post.like} people liked this
+            </span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">
+              {" "}
+              {post.comment} comments
+            </span>
+          </div>
         </div>
       </div>
     </div>
