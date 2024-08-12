fa78489c1fccac3b1207fb48626197c76ae43b03
Sophia Katsuno <sophia.katsuno@gmail.com>
2024-08-12 14:19:58 -0700

    incorporate profile component and update rightbar

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,44 @@
+import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+
+export default function Profile() {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="assets/post/3.jpeg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="assets/person/profile.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">Sophia Katsuno</h4>
+              <span className="profileInfoDesc">
+                Hi everyone! I am a full-stack web developer based in
+                Vancouver, BC. Thanks for stopping by along one of my
+                many coding adventures!
+              </span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
