03c0a9242157f2748602bc8a0bbc8e419c75add0
Sophia Katsuno <sophia.katsuno@gmail.com>
2024-08-12 11:32:14 -0700

    add rightbar component

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,6 +2,35 @@ import "./rightbar.css";
 
 export default function Rightbar() {
   return (
-    <div className="rightbar">Rightbar</div>
-  )
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <span className="birthdayText">
+            {" "}
+            <b>Mark Zuckerberg</b> and <b>2 others</b> have birthdays
+            today
+          </span>
+        </div>
+        <img src="assets/ad.png" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline" ></span>
+            </div>
+            <span className="John Carter">
+              John Doe
+            </span>
+            
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
