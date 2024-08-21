2e2017a9c1c39aaf235e7fd379d920619b387101
Sophia Katsuno <sophia.katsuno@gmail.com>
2024-08-21 15:18:35 -0700

    create user router

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -26,7 +29,9 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 // Start server
 app.listen(8800, () => {
