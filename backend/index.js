b95c8968b0042a6846ab23e5b9539f32c48695f6
Sophia Katsuno <sophia.katsuno@gmail.com>
2024-08-21 14:07:52 -0700

    connect mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
+
+dotenv.config();
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+  }
+}
+
+connectDB();
+
+
+// Start server
 app.listen(8800, () => {
-  console.log('Server is running on port 8800');
-}) 
+  console.log("Backend server is running on port 8800!");
+});
